1537085099fb5ef5d91599c88225fb1e7e773999
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2023-01-31 10:35:40 -0500

    Add static express server and scaffold for lottery app

--- express/static_server.mjs
+++ express/static_server.mjs
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log("another get request" + req.url);
   res.send('Hello World!')
 })
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
